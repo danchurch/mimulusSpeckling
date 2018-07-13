fcb9b5662d121b65808596aa77094279d2bda902
danchurch <danchurchthomas@gmail.com>
2018-07-13 15:09:56 -0700

    gimp scriptfu command line continued

--- make_polygons/make_spot_polygons.scm
+++ make_polygons/make_spot_polygons.scm
@@ -1,20 +1,30 @@
-;; try to get a script running:
-;; for the moment, this should just open a file and create 
-;; a new one with the new user given name
-
-(define (make-polygon inFile outFile) 
-
-    ;; vars
-    (let* ( 
-            (image (car (gimp-file-load RUN-NONINTERACTIVE inFile) ) )
+    (let* (
+            ;; load files, set vars
+            (image (car (gimp-file-load 
+                                0 
+                                "/Users/danthomas/Documents/speckling/demo/test_spots3.png" 
+                                "/Users/danthomas/Documents/speckling/demo/test_spots3.png") ) )
             (drawable (car (gimp-image-get-active-layer image)))
           )
-
-    ;; expressions
-    (gimp-file-save RUN-NONINTERACTIVE image drawable outFile outFile)
-
-    (gimp-image-delete image)
-    )
+            ;; do stuff
+            ;;1 select black/solid regions:
+            (gimp-image-select-color 
+                image
+                CHANNEL-OP-REPLACE
+                drawable
+                '(0 0 0) ;; black
+            )
+            ;;2 Convert selection to path
+            (plug-in-sel2path
+                RUN-NONINTERACTIVE
+                image
+                drawable
+            ) 
+            ;;3 save path to file
+        (gimp-vectors-export-to-file
+            image
+            "/Users/danthomas/Documents/speckling/make_polygons/spots_script_test.svg"
+            0 ;; all vectors
+        )
 )
-
 

2123e15aa251cc263c6bca46aa17b9e6aada0a82
danchurch <danchurchthomas@gmail.com>
2018-08-24 09:36:29 -0700

    revising polygon creation, try to find corners.

--- make_polygons/get_pols.py
+++ make_polygons/get_pols.py
@@ -49,3 +49,58 @@ for n, contour in enumerate(contours):
     np.savetxt(fname=polyname, X=contour.astype(int), delimiter=',')
 
 
+%%%%%%%%%%%%%%%%%%%%
+
+
+%% okay, the problems with the polygon digitization need to be fixed, one 
+%% way or another. 
+
+%% The problems are:
+
+%% 1) doug's color simplification sometimes caused cracks or leaks of photo 
+%% background color into the images. The digitization process followed these
+%% creaks and this broke up the petal polygons.
+
+%% 2) Holes in petal-spots are interpreted as additional spots, not as petal
+
+%% 3) Some petals were interpreted as entirely spot, usually when there were
+%% no significant spots at all on the petal.
+
+%% the second I can fix by tweaking our digitization algorithm. 1 and 3 are 
+%% problems deeper in the image analysis, from Doug's color simplication 
+%% process. I think. But #3 is a mystery to me. 
+
+
+%% of course, Arielle would like an edge statistic, too...
+
+%% let's look at #3 a little bit.
+
+%% some examples
+P428F1  
+P428F2  
+P430F2
+
+%% let's bring up the structures:
+
+wd = '/Users/danthomas/Documents/speckling/make_polygons/';
+objDir="/Users/danthomas/Documents/speckling/make_polygons/matObjW_pol/";
+dougDir = "/Users/danthomas/Documents/speckling/dougRaster/Rotated_and_Cropped/";
+fname = 'P428F2';
+
+load(objDir + fname + ".mat")
+
+petal = Petals.Polys.right.petal(1);
+
+spots = Petals.Polys.right.spots;
+
+hold off;
+plot(petal)
+hold on;
+plot(spots(1))
+%%
+
+hold off;
+plot(spots(1))
+
+hold off;
+plot(spots(2))

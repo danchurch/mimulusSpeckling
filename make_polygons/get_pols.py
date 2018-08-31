73e97c27b92304fec83e98825299d963874240b3
danchurch <danchurchthomas@gmail.com>
2018-08-31 15:48:27 -0700

    working on conversion to geojson

--- make_polygons/get_pols.py
+++ make_polygons/get_pols.py
@@ -1,106 +1,123 @@
 #!/usr/bin/env python3
 
-import argparse, os
+import argparse, os, pickle
 import numpy as np
 import matplotlib.pyplot as plt
-from skimage import data, io, filters, measure
+import shapely.geometry as sg
+import shapely.affinity as sa
+from skimage import measure
 
-## instantiate the argparser
-parser = argparse.ArgumentParser()
-## deal with arguments:
-parser.add_argument('file', 
-            help="Name of the .csv file from matlab that contains grayscale information about a petal.")
-args = parser.parse_args()
+def digitizePols(file):
+    ## pad margins of image and get the contour of the petal shape
+    petal = np.genfromtxt (file, delimiter=",")
+    petal_marg = np.insert(petal, petal.shape[1], 1, 1)
+    petal_marg = np.insert(petal_marg, 0, 1, 1)
+    petal_marg = np.insert(petal_marg, petal.shape[0], 1, 0)
+    petal_marg = np.insert(petal_marg, 0, 1, 0)
+    Pcontours = measure.find_contours(petal_marg, 0)
+    ## gotta ditch <3 points, they are lines
+    polys = [ i for i in Pcontours if len(i) > 3 ]
+    return(polys)
 
-## get our image in...
-aa  = np.genfromtxt(args.file, delimiter = ",")
+def getPetGeoInfo(pet):
+    aa = sg.asPolygon(pet)
+    area = aa.area
+    scalar = area**(-1/2)
+    center = aa.centroid
+    centerCoor = (center.x, center.y)
+    return(scalar, centerCoor)
 
-## set up our directory for saving files
+def stand(pol, scale, cent):
+    aa = sg.asPolygon(pol)
+    trans = sa.translate(aa, (-1*cent[0]), (-1*cent[1]))
+    scaled = sa.scale(trans, xfact=scale, yfact=scale, origin = (0,0))
+    return(scaled)
 
-## name of raster image (a petal or a spots grayscale matrix from matlab)
-imageName = args.file[:-4] ## no file extension
-#imageName = argsfile[:-4] ## no file extension
+def findZones(standPol, percent, simp=0.05):
+    center = standPol
+    rad = 0
+    while center.area > percent:
+        center = standPol.buffer(rad)
+        rad -= .001
 
-
-## name of folder, based on image
-cwd = os.getcwd()
-fullFolderName = cwd + "/" + os.path.basename(imageName)
-## make a new directory for the CSVs
-## we will make at the end of all this:
-os.makedirs(fullFolderName, exist_ok=True)
-
-aR = aa.shape[0] ## number of rows
-aC = aa.shape[1] ## number of cols
-
-## add one pixel of white to rows:
-aa_marg = np.insert(aa, aR, 1, 0)
-aa_marg = np.insert(aa_marg, 0, 1, 0)
-## and columns:
-aa_marg = np.insert(aa_marg, aC, 1, 1)
-aa_marg = np.insert(aa_marg, 0, 1, 1)
-
-## find the contours:
-contours = measure.find_contours(aa_marg, 0)
-
-## save them out:
-
-for n, contour in enumerate(contours):
-    polyname = fullFolderName + "/" + os.path.basename(imageName) + "_poly" + str(n) + ".csv"
-    np.savetxt(fname=polyname, X=contour.astype(int), delimiter=',')
-
-
-%%%%%%%%%%%%%%%%%%%%
-
-
-%% okay, the problems with the polygon digitization need to be fixed, one 
-%% way or another. 
-
-%% The problems are:
-
-%% 1) doug's color simplification sometimes caused cracks or leaks of photo 
-%% background color into the images. The digitization process followed these
-%% creaks and this broke up the petal polygons.
-
-%% 2) Holes in petal-spots are interpreted as additional spots, not as petal
-
-%% 3) Some petals were interpreted as entirely spot, usually when there were
-%% no significant spots at all on the petal.
-
-%% the second I can fix by tweaking our digitization algorithm. 1 and 3 are 
-%% problems deeper in the image analysis, from Doug's color simplication 
-%% process. I think. But #3 is a mystery to me. 
+    marg = sg.polygon.Polygon(
+            standPol.exterior,
+            holes = [center.exterior.coords])
+    simPol = marg.simplify(simp)
+    simPolA = np.array(simPol.exterior.xy).transpose()
+    simPolA[:,0].argsort()
+    simPolAsorted = simPolA[simPolA[:,1].argsort()[::-1]]
+    outCorners = simPolAsorted[0:2]
+    simPolB = np.array(simPol.interiors[0].xy).transpose()
+    simPolB[:,0].argsort()
+    simPolBsorted = simPolB[simPolB[:,1].argsort()[::-1]]
+    inCorners = simPolBsorted[0:2,]
+    inCorners = np.flipud(inCorners)
+    tRap = np.concatenate((outCorners,inCorners))
+    tRapPoly = sg.polygon.Polygon(tRap)
+    tBuff = tRapPoly.buffer(0.1)
+    noTrap = marg.difference(tRapPoly)
+    notInTrap = [ i for i in noTrap if i.within(tBuff) ]
+    mpNotInTrap = sg.multipolygon.MultiPolygon(notInTrap)
+    margInTrap = tRapPoly.intersection(marg)
+    throat = margInTrap.union(mpNotInTrap )
+    edge = marg.difference(throat)
+    return(center, edge, throat)
 
 
-%% of course, Arielle would like an edge statistic, too...
+if __name__ == "__main__":
 
-%% let's look at #3 a little bit.
 
-%% some examples
-P428F1  
-P428F2  
-P430F2
+    ## deal with arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('folder', 
+                help=("Name of folder that contains two files:"
+                      " that contains grayscale petal outlint and spot "
+                        "information in the form of CSVs."))
+    parser.add_argument('centerSize', 
+                help=("give the proportion of the middle of the flower"
+                      " you would like to call Center Zone, from 0.01"
+                      " to 0.99."),
+                type=float)
+    parser.add_argument('destination', 
+                help=("Folder where you would like this file."))
 
-%% let's bring up the structures:
 
-wd = '/Users/danthomas/Documents/speckling/make_polygons/';
-objDir="/Users/danthomas/Documents/speckling/make_polygons/matObjW_pol/";
-dougDir = "/Users/danthomas/Documents/speckling/dougRaster/Rotated_and_Cropped/";
-fname = 'P428F2';
+    args = parser.parse_args()
 
-load(objDir + fname + ".mat")
 
-petal = Petals.Polys.right.petal(1);
+    os.chdir(args.folder)
+    aa = os.listdir()
+    for i in aa:
+        if "petal" in i:
+            petPol = digitizePols(i)[0] 
+        elif "spots" in i:
+            spotPol = digitizePols(i)
+    scale, cent = getPetGeoInfo(petPol)
+    standPet = stand(petPol, scale, cent)
+    standSpot = [ stand(i, scale, cent) for i in spotPol ]
+    center, edge, throat = findZones(standPet, args.centerSize, 0.07)
+    
+    polys = {
+        'standPet': standPet,
+        'standSpot': standSpot,
+        'center': center,
+        'edge': edge,
+        'throat': throat
+                }
+   
+    here = os.getcwd()
+    petalName = os.path.basename(here)
+    flowerName = os.path.basename(os.path.dirname(here)) 
+    outFileName = ( args.destination + "/" 
+                    + flowerName + "_" 
+                    + petalName 
+                    + "_polys.p")
 
-spots = Petals.Polys.right.spots;
+    output = open(outFileName, "wb")
 
-hold off;
-plot(petal)
-hold on;
-plot(spots(1))
-%%
+    with open(outFileName, "wb") as output:
+        pickle.dump( polys, output )
 
-hold off;
-plot(spots(1))
+#####################
 
-hold off;
-plot(spots(2))

832f5266a60d308fd79fd3d88f6247e61fd99631
danchurch <danchurchthomas@gmail.com>
2018-09-20 14:55:00 -0700

    Josh/Melia mapping module done

--- polygon_traits/sortJoshCSVs.py
+++ polygon_traits/sortJoshCSVs.py
@@ -1,40 +1,85 @@
-import argparse, os
+#!/usr/bin/env python3
+
+import argparse, os, csv, pickle
 import numpy as np
 import pandas as pd
 
-## we want a script that assigns Josh's sample numbers to our plant 
-## IDs, and duplicates where necessary (some plants will end up 
-## with >1 row because they had multiple samples)
 
-## accepts two imputs: 
+def makeMDict(mappFile):
+    ''' make a dictionary from our map of Josh's samples'''
+    mapp =  pd.read_csv(mappFile, header=None, dtype=str)
+    mapp.columns = 'Sample', 'Plant'
+    mapp.set_index('Sample', inplace = True)
+    mappDic = mapp.T.to_dict('list')
+    return(mappDic)
 
-## 1) a CSV with two columns: 1 - Josh's samples, 2 - name of Plant to which that sample belongs
+def makePDict(phenoFile):
+    ''' make a dictionary from our phenotype CSV'''
+    pheno =  pd.read_csv(phenoFile)
+    ## change plant names to match index, get rid of "P":
+    pheno.plantName = pheno.plantName.str.replace('P','')
+    pheno.set_index('plantName', inplace=True)
+    phenDic = pheno.to_dict('index')
+    return(phenDic)
 
-## 2) Our phenotype data, by plant.
+def makeNewDf(PDict, MDict):
+    ''' make a dataframe from the mapping and phenotype dictionaries'''
+    bagOfDicts = []
+    for i in MDict:
+        newRow = {'sample':i,'plantName':MDict[i][0]}
+        oldRow = PDict[MDict[i][0]]
+        mergedD = {**newRow , **oldRow}
+        bagOfDicts.append(mergedD)
+    pandaDict = pd.DataFrame(bagOfDicts)
+    pandaDict = pandaDict.set_index('plantName').reset_index()
+    pandaDict = pandaDict.set_index('sample')
+    return(pandaDict)
 
-## This will output a phenotype spreadsheet with josh's sample names correctly
-## mapped to 
 
-## additionally, if more than one sample exists, a new row will be created for that plant
-
-## so the sample unit here is one of Josh's samples
-
-## we'll put a default dictionary here in the script but allow the user to input a
-## new one as a csv in case this becomes outdated:
-
+## deal with command line arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("Phenotype data",
+parser.add_argument("phenoData",
                 type=str,
                 help=('CSV, of our phenotype data.'
                         ' Rows are plants.')
                 )
+
 parser.add_argument("--map", 
             type=str,
             help= ('CSV, with the first'
             ' column are Josh\'s sample names'
             ' and the second columns are plant'
-            ' names. Rows are Josh\'s samples') 
+            ' names. Don\t use column names.'
+            ' Rows are Josh\'s samples.'),
+            default=(
+                    os.getcwd() + 
+                    "/" + "mapSample2plant.csv"
+                            )
+            )
+
+parser.add_argument("--outputName",
+            type=str,
+            help=('Name of the reformatted CSV file.'
+            ' Goes to stdout if no name given.'),
                 )
+args = parser.parse_args()
 
+## organize names a litte:
+phenoFile = args.phenoData
+mappFile = args.map
+if args.outputName is None:
+    OutCSV=('modified_' + args.phenoData)
+    OutPickle = OutCSV.replace("csv","p")
+else:
+    OutCSV = args.outputName
+    OutPickle = OutCSV.replace("csv","p")
 
-args = parser.parse.args()
+## run our functions
+ph = makePDict(phenoFile)
+ma = makeMDict(mappFile)
+df = makeNewDf(ph,ma)
+
+## create csv and pickled dataframe:
+df.to_csv(OutCSV, na_rep='NaN')
+
+pickle.dump(df, open(OutPickle, "wb"))

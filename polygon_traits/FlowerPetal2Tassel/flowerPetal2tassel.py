97ebe200f86a561592a6bb969ad190e5cdb3c585
danchurch <danchurchthomas@gmail.com>
2018-10-29 13:55:30 -0700

    melia no-throat analysis complete?

---
+++ polygon_traits/FlowerPetal2Tassel/flowerPetal2tassel.py
@@ -0,0 +1,30 @@
+
+import os, pickle
+import numpy as np
+import pandas as pd
+
+##get our python file for this data (could use the CSV instead)
+pickleFile="automated_stats_29.10.2018.p"
+rawDataDF = pickle.load(open(pickleFile, "rb"))
+## clean up 
+del rawDataDF['geojson']
+## split out the petal types
+uppersRaw = rawDataDF[rawDataDF.petalName != 'right']
+lowerRaw = rawDataDF[rawDataDF.petalName == 'right']
+## group by flowers for upper petal, average the upper petals
+uppersMean = uppersRaw.groupby(['plantName','flowerName']).mean()
+## clean up new columns 
+uppersMean.reset_index(inplace = True)
+## add petalName back in (dropped above)
+uppersMean['petalName'] = 'upper'
+lowerRaw['petalName'] = 'lower'
+## concatenate by row
+combRaw = pd.concat([ uppersMean, lowerRaw ], sort=True)
+## take the average of each petal type nested within each plant
+plantDf = combRaw.groupby(['plantName','petalName']).mean()
+## localized transpose
+meliaData = plantDf.unstack(level=1)
+## collapse the hierarchical labels
+newName = ['_'.join(col).strip() for col in meliaData.columns.values] 
+meliaData.columns = newName
+meliaData.to_csv('automatedResuls_forTassle_29.10.2018.csv', na_rep='NaN')

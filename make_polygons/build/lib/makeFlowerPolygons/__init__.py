6108bffaa5c1cd02ec3c085f79928d2bf5a73191
danchurch <danchurchthomas@gmail.com>
2019-02-12 12:37:35 -0800

    trying to git stuff onto melia's machine

--- make_polygons/build/lib/makeFlowerPolygons/__init__.py
+++
@@ -1,1 +0,0 @@
-name= "flowerPetal_pkg"
--- make_polygons/package/setup.py
+++ make_polygons/package/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="FlowerPetal-dcthom",
+    name="makeFlowerPolygons-dcthom",
     version="0.0.1",
     author="Daniel Thomas",
     author_email="thomasdc@whitman.edu",

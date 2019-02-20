320fcebec778f2e85fc8d6eb704aeaf8af7a41d2
danchurch <danchurchthomas@gmail.com>
2019-02-20 13:16:54 -0800

    patch for bounding boxes to fix the spotestimate bug

---
+++ make_polygons/scripts/fixBB.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import argparse, json 
+from makeFlowerPolygons import geojsonIO
+from makeFlowerPolygons.get_spots import parseDougMatrix, digitizePols, cleanPetal
+
+## deal with args:
+parser = argparse.ArgumentParser()
+parser.add_argument('geojson')
+parser.add_argument('csv')
+parser.add_argument('-o', '--outFileName', default=None)
+args = parser.parse_args()
+
+if not args.outFileName:
+    outFileName=args.geojson
+else:
+    outFileName=args.outFileName
+
+## script
+
+(petal,spots,center,edge,throat,
+    spotEstimates, photoBB,
+                scalingFactor) = geojsonIO.parseGeoJson(args.geojson)
+ 
+_, petalCSV, _ = parseDougMatrix(args.csv)
+rawPetal = digitizePols(petalCSV)
+clePetal = cleanPetal(rawPetal)
+## get our new photoBB
+photoBB = list(clePetal.bounds)
+
+## write it out
+geoDict = geojsonIO.writeGeoJ(petal, spots,
+                                center, edge, throat,
+                                spotEstimates, photoBB, scalingFactor)
+
+with open(outFileName, 'w') as fp:
+    json.dump(geoDict, fp)

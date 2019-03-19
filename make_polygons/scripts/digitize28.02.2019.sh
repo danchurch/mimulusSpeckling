55a5154d963b0648c8e0f80e74107578a7869779
danchurch <danchurchthomas@gmail.com>
2019-03-19 10:46:40 -0700

    holes in spots now allowed?

--- make_polygons/scripts/digitize28.02.2019.sh
+++ make_polygons/scripts/digitize28.02.2019.sh
@@ -11,33 +11,33 @@ outDir='/Users/danthomas/Documents/bigPDF'
 jpgDirectory=$repo"dougRaster/Rotated_and_Cropped/plate3"
 cd $wd
 
-#echo "########################################"
-#echo "Making petals and spots"
-#echo ""
-#echo ""
-#
-#aa=$(find $wd -name "*melted.csv")
-#
-#for i in ${aa[@]}; do
-#    echo "Now making petal and spots for: "$i
-#    $get_spots $i
-#done
-#
-#
-#
-#echo ""
-#echo "########################################"
-#echo "Calling zones"
-#echo ""
-#echo ""
-#
-#bb=$(find $wd -name "*geojson")
-#
-#for i in ${bb[@]}; do
-#    echo $i
-#    $get_zones $i 0.5
-#done
-#
+echo "########################################"
+echo "Making petals and spots"
+echo ""
+echo ""
+
+aa=$(find $wd -name "*melted.csv")
+
+for i in ${aa[@]}; do
+    echo "Now making petal and spots for: "$i
+    $get_spots $i
+done
+
+
+
+echo ""
+echo "########################################"
+echo "Calling zones"
+echo ""
+echo ""
+
+bb=$(find $wd -name "*geojson")
+
+for i in ${bb[@]}; do
+    echo $i
+    $get_zones $i 0.5
+done
+
 echo ""
 echo "########################################"
 echo "Creating PDFs"
